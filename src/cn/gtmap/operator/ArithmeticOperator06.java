8605f3b9e056f33934d4288e4eb3756b5b75413b
陶文猛 <15152806231@163.com>
2019-02-13 18:03:19 +0800

    自增、自减

---
+++ src/cn/gtmap/operator/ArithmeticOperator06.java
@@ -0,0 +1,27 @@
+package cn.gtmap.operator;
+/**
+ * 7¡¢×ÔÔö  ×Ô¼õ
+ * @author Administrator
+ *
+ */
+public class ArithmeticOperator06 {
+
+	public static void main(String[] args) {
+		int i = 5;
+		i++;
+		System.out.println(i);
+		
+		int j = 10;
+		j--;
+		System.out.println(j);
+		
+		int k = 15;
+		++k;
+		System.out.println(k);
+		
+		int l = 20;
+		--l;
+		System.out.println(l);
+	}
+
+}
