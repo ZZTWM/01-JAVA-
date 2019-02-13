50432240db497e314d0a55e84b492ee4347aa637
陶文猛 <15152806231@163.com>
2019-02-13 17:21:56 +0800

    块

---
+++ src/cn/gtmap/variable/Block.java
@@ -0,0 +1,14 @@
+package cn.gtmap.variable;
+/**
+ * 块
+ * 从{ 开始 到对应的} 结束，即一个块
+ * @author Administrator
+ *
+ */
+public class Block {//类对应的块
+
+	public static void main(String[] args) {//主方法对应的块
+		System.out.println("我爱中国");
+	}
+
+}
