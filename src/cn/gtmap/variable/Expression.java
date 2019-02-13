daee7ac29fa7274d937c6b1f3073c341a8986ed3
陶文猛 <15152806231@163.com>
2019-02-13 17:18:41 +0800

    表达式

---
+++ src/cn/gtmap/variable/Expression.java
@@ -0,0 +1,27 @@
+package cn.gtmap.variable;
+/**
+ * 表达式
+ * @author Administrator
+ *
+ */
+public class Expression {
+
+	public static void main(String[] args) {
+		/**
+		 * 1、以;结尾的一段代码，即为一个表达式
+		 */
+		int i= 5;
+		System.out.println(i);
+		
+		/**
+		 * 2、; 也是一个完整的表达式
+		 */
+		//一个空;也是一个表达式
+		;
+		;
+		;
+		;
+		;
+	}
+
+}
