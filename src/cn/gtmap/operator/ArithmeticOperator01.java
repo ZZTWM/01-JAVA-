f2285775dc87d943ad2fdde108f4869c855c47d3
陶文猛 <15152806231@163.com>
2019-02-13 17:30:35 +0800

    操作符：算数操作符-基本算数操作符

---
+++ src/cn/gtmap/operator/ArithmeticOperator01.java
@@ -0,0 +1,21 @@
+package cn.gtmap.operator;
+/**
+ * 操作符：算数操作符
+ * @author Administrator
+ *
+ */
+public class ArithmeticOperator01 {
+
+	public static void main(String[] args) {
+		/**
+		 * 1、基本算数操作符
+		 */
+		int i = 10;
+		int j = 5;
+		int a = i + j;
+		int b = i - j;
+		int c = i * j;
+		int d = i / j;
+	}
+
+}
