c82d91e276e1cab1f05c9908a4d6b91f4cf308eb
陶文猛 <15152806231@163.com>
2019-02-13 16:58:15 +0800

    什么是变量

---
+++ src/cn/gtmap/variable/WhatIsVariable.java
@@ -0,0 +1,13 @@
+package cn.gtmap.variable;
+/**
+ * 什么是变量
+ * @author Administrator
+ *
+ */
+public class WhatIsVariable {
+
+	public static void main(String[] args) {
+		int year = 1949;
+	}
+
+}
