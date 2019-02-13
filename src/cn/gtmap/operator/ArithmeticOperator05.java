356903d76b9b06fd3260bbb74ca79f34ecc098c4
陶文猛 <15152806231@163.com>
2019-02-13 17:57:17 +0800

    取模/取模

---
+++ src/cn/gtmap/operator/ArithmeticOperator05.java
@@ -0,0 +1,15 @@
+package cn.gtmap.operator;
+/**
+ * 取模/取余：%
+ * @author Administrator
+ *
+ */
+public class ArithmeticOperator05 {
+
+	public static void main(String[] args) {
+		int i = 5;
+		int j = 2;
+		System.out.println(i%j);//输出为1
+	}
+
+}
