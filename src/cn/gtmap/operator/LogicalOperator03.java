dae16e8fe1b595d9ed8492be9f91f9a7a23dbd4e
陶文猛 <15152806231@163.com>
2019-02-14 09:23:14 +0800

    逻辑操作符：取反

---
+++ src/cn/gtmap/operator/LogicalOperator03.java
@@ -0,0 +1,18 @@
+package cn.gtmap.operator;
+/**
+ * Âß¼­²Ù×÷·û
+ * @author Administrator
+ *
+ */
+public class LogicalOperator03 {
+	/**
+	 * 3¡¢È¡·´£¡
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		boolean b = true;
+		System.out.println(b);//true
+		System.out.println(!b);//false
+	}
+
+}
