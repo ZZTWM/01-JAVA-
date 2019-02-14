d3cbb39d7318438434497b8be684479bbc45cbb8
陶文猛 <15152806231@163.com>
2019-02-14 08:43:33 +0800

    关系操作符

---
+++ src/cn/gtmap/operator/RelationalOperator01.java
@@ -0,0 +1,20 @@
+package cn.gtmap.operator;
+/**
+ * 1¡¢¹ØÏµ²Ù×÷·û
+ * @author Administrator
+ *
+ */
+public class RelationalOperator01 {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 6;
+		int c = 5;
+		System.out.println(a > b);//false
+		System.out.println(a >= c);//true
+		
+		System.out.println(a == b);//false
+		System.out.println(a != b);//true
+	}
+
+}
