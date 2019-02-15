431f9e46d668d0f4c389d42a3483d0f8a2f91ca5
陶文猛 <15152806231@163.com>
2019-02-15 08:49:36 +0800

    创建数组

---
+++ src/cn/gtmap/array/BuildArray02.java
@@ -0,0 +1,21 @@
+package cn.gtmap.array;
+/**
+ * 创建数组
+ * @author Administrator
+ *
+ */
+public class BuildArray02 {
+	/**
+	 * 2、创建数组
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//声明一个引用
+		int[] a;
+		//创建一个长度是5的数组，并且使用引用a指向该数组
+		a = new int[5];
+		
+		int[] b = new int[5];//声明的同时，指向一个数组
+	}
+
+}
