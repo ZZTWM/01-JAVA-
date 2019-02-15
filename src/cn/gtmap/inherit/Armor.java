fa1b4899caa84f22fa554e63e24b55b46cdafb4d
陶文猛 <15152806231@163.com>
2019-02-15 16:44:08 +0800

    继承

---
+++ src/cn/gtmap/inherit/Armor.java
@@ -0,0 +1,12 @@
+package cn.gtmap.inherit;
+/**
+ * 护甲类，不继承物品类
+ * 独立设计 name和price属性
+ * 同时多了一个属性 ac 护甲
+ *
+ */
+public class Armor {
+	String name;//名称
+	int price;//价格
+	int ac;//护甲值
+}
---
+++ src/cn/gtmap/inherit/Item.java
@@ -0,0 +1,10 @@
+package cn.gtmap.inherit;
+
+/**
+ * 物品类
+ *
+ */
+public class Item {
+	String name;//名称
+	int price;//价格
+}
