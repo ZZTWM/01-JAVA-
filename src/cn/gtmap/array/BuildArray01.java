0327948edbc2ecdfdda26a6365f13172fbd57112
陶文猛 <15152806231@163.com>
2019-02-15 08:45:55 +0800

    创建数组：声明数组

---
+++ src/cn/gtmap/array/BuildArray01.java
@@ -0,0 +1,18 @@
+package cn.gtmap.array;
+/**
+ * 创建数组
+ * @author Administrator
+ *
+ */
+public class BuildArray01 {
+	/**
+	 * 1、声明数组
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int[] a;
+		
+		int b[];
+	}
+
+}
