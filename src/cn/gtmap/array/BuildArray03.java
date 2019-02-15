316db65594c75b83f295d99e74a773c0c65d774f
陶文猛 <15152806231@163.com>
2019-02-15 08:54:23 +0800

    创建数组：访问数组

---
+++ src/cn/gtmap/array/BuildArray03.java
@@ -0,0 +1,28 @@
+package cn.gtmap.array;
+/**
+ * 创建数组
+ * @author Administrator
+ *
+ */
+public class BuildArray03 {
+	/**
+	 * 访问数组
+	 */
+	public static void main(String[] args) {
+		int[] a;
+		a = new int[5];
+		a[0] = 1; //下标0，代表数组里的第一个数
+		a[1] = 2;
+		a[2] = 3;
+		a[3] = 4;
+		a[4] = 5;
+		
+		int[] b = new int[5];
+		b[0] = 1;//下标0，代表数组里的第一个数
+		b[1] = 2;
+		b[2] = 3;
+		b[3] = 4;
+		b[4] = 5;
+	}
+
+}
