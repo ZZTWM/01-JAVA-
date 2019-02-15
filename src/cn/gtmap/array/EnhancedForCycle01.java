a640a0182cfacce6a729772f706327963d0997b0
陶文猛 <15152806231@163.com>
2019-02-15 11:45:07 +0800

    增强的for循环

--- src/cn/gtmap/array/EnhancedForCycle01.java
+++ src/cn/gtmap/array/EnhancedForCycle01.java
@@ -3,7 +3,7 @@ package cn.gtmap.array;
  * 增强型for循环
  *
  */
-public class EnhancedForCycle {
+public class EnhancedForCycle01 {
 
 	public static void main(String[] args) {
 		int[] a = new int[]{18,62,68,82,65,9};
