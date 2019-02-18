651d315b449b3e4034383294337424cefdfb5cea
陶文猛 <15152806231@163.com>
2019-02-18 11:01:14 +0800

    练习：构造方法

---
+++ src/cn/gtmap/constructors/Supporter.java
@@ -0,0 +1,30 @@
+package cn.gtmap.constructors;
+/**
+ * 练习：构造方法
+ * @author Administrator
+ *
+ */
+public class Supporter {
+	
+    String name; //姓名
+    
+    float hp; //血量
+       
+    float armor; //护甲
+       
+    int moveSpeed; //移动速度
+    
+    public Supporter(String heroName,float heroHp,float heroArmor,int heroMoveSpeed){
+    	name = heroName;
+    	hp = heroHp;
+    	armor = heroArmor;
+    	moveSpeed = heroMoveSpeed;
+    }
+	
+	public static void main(String[] args) {
+		
+		Supporter soraka = new Supporter("索拉卡", 318, 20.5f, 305);
+		
+	}
+
+}
