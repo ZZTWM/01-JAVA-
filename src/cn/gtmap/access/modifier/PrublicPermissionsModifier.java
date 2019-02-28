d3b2c44132209f17e9050645e1a851018894b8d3
陶文猛 <15152806231@163.com>
2019-02-28 10:15:57 +0800

    public 公共的

---
+++ src/cn/gtmap/access/modifier/PrublicPermissionsModifier.java
@@ -0,0 +1,17 @@
+package cn.gtmap.access.modifier;
+
+public class PrublicPermissionsModifier {
+	
+	private int id;
+	
+    // public的属性 name
+    // 自己可以访问
+ 
+    // 同包子类可以继承
+    // 不同包子类可以继承
+ 
+    // 同包类可以访问
+    // 不同包类可以访问
+	public String name;
+	
+}
