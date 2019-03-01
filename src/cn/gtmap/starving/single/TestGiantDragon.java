f9ff59f800a91fdc77128be3023ea700c7c3a6be
陶文猛 <15152806231@163.com>
2019-03-01 10:41:11 +0800

    饿汉式单例模式

---
+++ src/cn/gtmap/starving/single/TestGiantDragon.java
@@ -0,0 +1,19 @@
+package cn.gtmap.starving.single;
+
+public class TestGiantDragon {
+	
+	public static void main(String[] args) {
+		//通过new实例化会报错
+		//GiantDragon g = new GiantDragon();
+		//只能通过getInstance得到对象
+		
+		GiantDragon g1 = GiantDragon.getInstance();
+		GiantDragon g2 = GiantDragon.getInstance();
+		GiantDragon g3 = GiantDragon.getInstance();
+		
+		System.out.println(g1 == g2);
+		
+		System.out.println(g2 == g3);
+	}
+	
+}
