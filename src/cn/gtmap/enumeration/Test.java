a284884c362ae4b631b5dcf41451a302af7b9091
陶文猛 <15152806231@163.com>
2019-03-20 10:13:20 +0800

    遍历枚举

---
+++ src/cn/gtmap/enumeration/Test.java
@@ -0,0 +1,15 @@
+package cn.gtmap.enumeration;
+/**
+ * ±éÀúÃ¶¾Ù
+ * @author Administrator
+ *
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		for (Season s : Season.values()) {
+			System.out.println(s);
+		}
+	}
+
+}
