476f980d1f9017bc892c172000c72ecbf79d3dad
陶文猛 <15152806231@163.com>
2019-03-20 10:44:40 +0800

    练习接口：Support类实现Healer接口，继承Hero类

---
+++ src/cn/gtmap/interface_/inherit/Healer.java
@@ -0,0 +1,5 @@
+package cn.gtmap.interface_.inherit;
+
+public interface Healer {
+	public void heal();
+}
---
+++ src/cn/gtmap/interface_/inherit/Support.java
@@ -0,0 +1,10 @@
+package cn.gtmap.interface_.inherit;
+
+public class Support extends Hero implements Healer {
+
+	@Override
+	public void heal() {
+		System.out.println("÷Œ¡∆’ﬂ");
+	}
+
+}
