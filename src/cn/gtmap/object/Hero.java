f07a7ffab8876fc91a45ed5a3f94eb32406522b1
陶文猛 <15152806231@163.com>
2019-03-28 17:02:46 +0800

    接口与继承：object类

---
+++ src/cn/gtmap/object/Hero.java
@@ -0,0 +1,50 @@
+package cn.gtmap.object;
+
+public class Hero {
+    String name; //姓名
+    
+    float hp; //血量
+        
+    float armor; //护甲
+        
+    int moveSpeed; //移动速度
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getHp() {
+		return hp;
+	}
+
+	public void setHp(float hp) {
+		this.hp = hp;
+	}
+
+	public float getArmor() {
+		return armor;
+	}
+
+	public void setArmor(float armor) {
+		this.armor = armor;
+	}
+
+	public int getMoveSpeed() {
+		return moveSpeed;
+	}
+
+	public void setMoveSpeed(int moveSpeed) {
+		this.moveSpeed = moveSpeed;
+	}
+
+	@Override
+	public String toString() {
+		return "Hero [name=" + name + ", hp=" + hp + ", armor=" + armor + ", moveSpeed=" + moveSpeed + "]";
+	}
+    
+    
+}
---
+++ src/cn/gtmap/object/Test.java
@@ -0,0 +1,12 @@
+package cn.gtmap.object;
+
+public class Test {
+	public static void main(String[] args) {
+		Hero garen = new Hero();
+		garen.name = "¸ÇÂ×";
+		garen.armor = 34.5f;
+		garen.hp = 459.4f;
+		garen.moveSpeed = 313;
+		System.out.println(garen);
+	}
+}
