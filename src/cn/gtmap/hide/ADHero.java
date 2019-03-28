10509918469b181bba70311a4939e0c7d9ef75b4
陶文猛 <15152806231@163.com>
2019-03-28 16:30:03 +0800

    接口与继承：隐藏

---
+++ src/cn/gtmap/hide/ADHero.java
@@ -0,0 +1,7 @@
+package cn.gtmap.hide;
+
+public class ADHero extends Hero{
+	public static void battleWin(){
+		System.out.println("ad hero battle win");
+	}
+}
---
+++ src/cn/gtmap/hide/Test.java
@@ -0,0 +1,8 @@
+package cn.gtmap.hide;
+
+public class Test {
+	public static void main(String[] args) {
+		Hero.battleWin();
+		ADHero.battleWin();
+	}
+}
