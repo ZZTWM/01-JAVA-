90396ab5df392e536d8bd92b666f87ae983c0ff6
陶文猛 <15152806231@163.com>
2019-03-28 14:58:22 +0800

    明确引用类型与对象类型的概念

---
+++ src/cn/gtmap/object_/casting/Hero.java
@@ -0,0 +1,12 @@
+package cn.gtmap.object_.casting;
+
+public class Hero {
+	
+	public String name;
+	protected float hp;
+	
+	public static void main(String[] args) {
+		Hero hero = new Hero();
+	}
+
+}
