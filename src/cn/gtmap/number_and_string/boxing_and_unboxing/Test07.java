2670842a00db62cd68808e4351cfeee105e9443b
陶文猛 <15152806231@163.com>
2019-04-03 14:56:17 +0800

    最大值，最小值

---
+++ src/cn/gtmap/number_and_string/boxing_and_unboxing/Test07.java
@@ -0,0 +1,32 @@
+package cn.gtmap.number_and_string.boxing_and_unboxing;
+
+/**
+ * 最大值，最小值
+ *
+ */
+public class Test07 {
+	public static void main(String[] args) {
+		
+		//byte最大值，最小值
+		System.out.println("byte最大值：" + Byte.MAX_VALUE);
+		System.out.println("byte最小值：" + Byte.MIN_VALUE);
+		
+		//short最大值，最小值
+		System.out.println("short最大值：" + Short.MAX_VALUE);
+		System.out.println("short最小值：" + Short.MIN_VALUE);
+		
+		//int最大值，最小值
+		System.out.println("int最大值：" + Integer.MAX_VALUE);
+		System.out.println("int最小值：" + Integer.MIN_VALUE);
+		
+		//long最大值，最小值
+		System.out.println("long最大值：" + Long.MAX_VALUE);
+		System.out.println("long最小值：" + Long.MIN_VALUE);
+		
+		System.out.println("float最大值：" + Float.MAX_VALUE);
+		System.out.println("flaot最小值：" + Float.MIN_VALUE);
+		
+		System.out.println("double最大值：" + Double.MAX_VALUE);
+		System.out.println("double最小值：" + Double.MIN_VALUE);
+	}
+}
