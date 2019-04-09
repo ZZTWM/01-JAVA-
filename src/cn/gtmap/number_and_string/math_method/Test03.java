39f89d5fde123fb4f1aba5ddc395078c8553f534
twm <15152806231@163.com>
2019-04-09 11:11:45 +0800

    数学方法：开方：Math.sqrt()

---
+++ src/cn/gtmap/number_and_string/math_method/Test03.java
@@ -0,0 +1,13 @@
+package cn.gtmap.number_and_string.math_method;
+
+public class Test03 {
+
+	public static void main(String[] args) {
+		
+		//开方
+		System.out.println(Math.sqrt(9));
+		//开方
+		System.out.println(Math.sqrt(16));
+	}
+
+}
