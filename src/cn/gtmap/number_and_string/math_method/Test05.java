fbdcdd1411b2f0f247f4947508957b194eebd872
twm <15152806231@163.com>
2019-04-09 11:17:37 +0800

    数学方法：π （Math.PI）自然常数（Math.E）

---
+++ src/cn/gtmap/number_and_string/math_method/Test05.java
@@ -0,0 +1,8 @@
+package cn.gtmap.number_and_string.math_method;
+
+public class Test05 {
+	public static void main(String[] args) {
+		System.out.println(Math.PI);
+		System.out.println(Math.E);
+	}
+}
