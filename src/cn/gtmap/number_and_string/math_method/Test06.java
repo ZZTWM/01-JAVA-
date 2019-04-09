920610b7eaf8d4f58750c338fcb62bd3b775e001
twm <15152806231@163.com>
2019-04-09 11:27:12 +0800

    数学方法：练习：自然常数

---
+++ src/cn/gtmap/number_and_string/math_method/Test06.java
@@ -0,0 +1,12 @@
+package cn.gtmap.number_and_string.math_method;
+
+public class Test06 {
+	public static void main(String[] args) {
+		System.out.println(Math.E);
+		
+		int n = Integer.MAX_VALUE;
+		
+		System.out.println(Math.pow(1 + 1d/n, n));
+		
+	}
+}
