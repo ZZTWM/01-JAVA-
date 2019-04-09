8752b30baa9b78db1d37b24a7ca56679b74d3832
twm <15152806231@163.com>
2019-04-09 11:03:38 +0800

    数学方法：随机数：Math.random()

---
+++ src/cn/gtmap/number_and_string/math_method/Test02.java
@@ -0,0 +1,12 @@
+package cn.gtmap.number_and_string.math_method;
+
+public class Test02 {
+	public static void main(String[] args) {
+		
+		//得到一个0-1之间的随机浮点数（取不到1）
+		System.out.println(Math.random());
+		//得到一个0-10之间的随机浮点数（取不到10）
+		System.out.println((Math.random()*10));
+		
+	}
+}
